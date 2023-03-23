815a8be73c602613b66004693f15c6d687c0ee1a
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-03-23 11:09:51 +0100

    added increase and decrease on items

--- js/contact.js
+++ js/contact.js
@@ -8,25 +8,31 @@ const messageError = document.querySelector("#messageError");
 
 function validateForm(event) {
 	event.preventDefault();
+	let isFormValid = true;
+
 	if (fullName.value.trim().length > 0) {
 		fullNameError.style.display = "none";
 	} else {
 		fullNameError.style.display = "block";
+		isFormValid = false;
 	}
 
 	if (email.value.trim().length > 0) {
 		emailError.style.display = "none";
 	} else {
 		emailError.style.display = "block";
+		isFormValid = false;
 	}
 	if (message.value.trim().length > 9) {
 		messageError.style.display = "none";
 	} else {
 		messageError.style.display = "block";
+		isFormValid = false;
 	}
-	// if (fullName && email && message) {
-	// 	window.location.href = "thankyou_contactform.html";
-	// }
+
+	if (isFormValid) {
+		window.location.href = "thankyou_contactform.html";
+	}
 }
 
 form.addEventListener("submit", validateForm);
