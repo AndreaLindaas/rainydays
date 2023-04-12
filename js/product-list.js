1b093a7b65dc0000a33ca8e0363c4863d9f2a8c6
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-04-12 20:18:26 +0200

    added product list

---
+++ js/product-list.js
@@ -0,0 +1,40 @@
+const url = "https://empty-maze.flywheelsites.com/wp-json/wc/store/products";
+const jacketList = document.querySelector(".jacket-list");
+
+async function getJackets() {
+	const response = await fetch(url);
+	const result = await response.json();
+	console.log(result);
+	showJackets(result);
+}
+
+function showJackets(result) {
+	for (let i = 0; i < result.length; i++) {
+		const jacket = `<li><a><img src="${result[i].images[0].src}" alt="${
+			result[i].images[0].alt
+		}"/><div> <p>${result[i].categories[1].name}</p> <p>${
+			result[i].name
+		}</p> <p>${result[i].prices.price / 100},-</p></div></a></li>`;
+		console.log(result);
+		jacketList.innerHTML += jacket;
+	}
+}
+
+getJackets();
+
+{
+	/* <ul class="jacket-list">
+				<li>
+					<a href="productpage.html"
+						><img
+							src="images/product/jacket1.jpg"
+							alt=" green Fleece jacket"
+						/>
+						<div>
+							<p>Rainydays Collection</p>
+							<p>Fleecejacket</p>
+							<p class="price">only 799,-</p>
+						</div></a
+					>
+				</li> */
+}
