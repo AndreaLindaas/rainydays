28e1204f061127168ca93e449c2ea6987db7ca20
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-04-13 20:05:28 +0200

    added product with id to productpage

--- js/productpage.js
+++ js/productpage.js
@@ -4,7 +4,13 @@ const closeModal = document.querySelector(".close");
 var img = document.querySelector(".img-productpage");
 var modalImg = document.querySelector(".modal-img");
 
-const url = "https://empty-maze.flywheelsites.com/wp-json/wc/store/products/";
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+const url =
+	"https://empty-maze.flywheelsites.com/wp-json/wc/store/products/" + id;
+const image = document.querySelector(".image-cont");
 
 img.onclick = function () {
 	modal.style.display = "block";
@@ -19,3 +25,34 @@ closeModal.onclick = function () {
 buyButton.onclick = function () {
 	alert("The product is added to cart");
 };
+
+async function getJacket() {
+	const response = await fetch(url);
+	const result = await response.json();
+	console.log(url);
+	showJacket(result);
+}
+
+function showJacket(jacket) {
+	showMainImage(jacket);
+	showText(jacket);
+	showDescription(jacket);
+}
+
+function showMainImage(jacket) {
+	const maninImage = (document.querySelector(".img-productpage").src =
+		jacket.images[0].src);
+}
+function showText(jacket) {
+	const namePrice = document.querySelector(".name-price");
+	const text = `<h1>${jacket.name}</h1><h2>${
+		jacket.categories[1].name
+	}</h2><h2 class="price">${jacket.prices.price / 100},-</h2>`;
+	namePrice.innerHTML = text;
+}
+function showDescription(jacket) {
+	const description = (document.querySelector(".description").innerHTML =
+		jacket.description);
+}
+
+getJacket();
