1da118ed2567d7ece8453054bca7bdbc3034cf8e
AndreaLindaas <112169120+AndreaLindaas@users.noreply.github.com>
2023-04-17 20:08:55 +0200

    added sort to product-list

--- js/product-list.js
+++ js/product-list.js
@@ -1,41 +1,55 @@
 const url = "https://empty-maze.flywheelsites.com/wp-json/wc/store/products";
 const jacketList = document.querySelector(".jacket-list");
+const sortHtml = document.querySelector("#sort");
+let jackets = [];
 
 async function getJackets() {
 	const response = await fetch(url);
 	const result = await response.json();
-	showJackets(result);
+	jackets = result;
+	showJackets();
 }
 
-function showJackets(result) {
-	for (let i = 0; i < result.length; i++) {
+function showJackets() {
+	sortJackets();
+	jacketList.innerHTML = "";
+	for (let i = 0; i < jackets.length; i++) {
 		const jacket = `<li><a href="productpage.html?id=${
-			result[i].id
-		}"><img src="${result[i].images[0].src}" alt="${
-			result[i].images[0].alt
-		}"/><div> <p>${result[i].categories[1].name}</p> <p>${
-			result[i].name
-		}</p> <p>${result[i].prices.price / 100},-</p></div></a></li>`;
+			jackets[i].id
+		}"><img src="${jackets[i].images[0].src}" alt="${
+			jackets[i].images[0].alt
+		}"/><div> <p>${jackets[i].categories[1].name}</p> <p>${
+			jackets[i].name
+		}</p> <p>${jackets[i].prices.price / 100},-</p></div></a></li>`;
 
 		jacketList.innerHTML += jacket;
 	}
 }
 
+function sortJackets() {
+	let sort = sortHtml.value;
+
+	if (sort === "asc") {
+		jackets = jackets.sort(compare);
+	} else if (sort === "desc") {
+		jackets = jackets.sort(compare);
+		jackets = jackets.reverse();
+	}
+}
+
+sortHtml.onchange = function () {
+	showJackets();
+};
+
 getJackets();
 
-{
-	/* <ul class="jacket-list">
-				<li>
-					<a href="productpage.html"
-						><img
-							src="images/product/jacket1.jpg"
-							alt=" green Fleece jacket"
-						/>
-						<div>
-							<p>Rainydays Collection</p>
-							<p>Fleecejacket</p>
-							<p class="price">only 799,-</p>
-						</div></a
-					>
-				</li> */
+//borrowed from the internet (stackoverflow)
+function compare(a, b) {
+	if (a.name < b.name) {
+		return -1;
+	}
+	if (a.name > b.name) {
+		return 1;
+	}
+	return 0;
 }
